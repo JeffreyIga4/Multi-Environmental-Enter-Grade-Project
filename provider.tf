07670839b903c52571c3978bfdcae1f0bb8159e6
Jeffrey Igala <jeffreyigala@MACBOOKPRO.station>
2026-01-18 18:08:38 +0000

    Needed files created

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+# configures the provider
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+}
+provider "azuread" {
+  
+}
